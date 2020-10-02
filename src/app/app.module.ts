a495cf4fd15f43d059ae97a0583a255b57ca946a
fateh mohamed <cr7141414@gmail.com>
2020-10-02 18:30:38 +0100

    delete ngxs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,28 +6,12 @@ import { AppComponent } from './app.component';
 import { TopNavComponent } from './src/app/top-nav/top-nav.component';
 import { HttpClientModule } from '@angular/common/http';
 import { SharedModule } from './src/app/shared/shared.module';
-import { NgxsModule } from '@ngxs/store';
 import { environment } from 'src/environments/environment';
-import {NgxsReduxDevtoolsPluginModule} from '@ngxs/devtools-plugin';
-import {NgxsLoggerPluginModule} from '@ngxs/logger-plugin';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    TopNavComponent
-  ],
-  imports: [
-    BrowserModule,
-    HttpClientModule,
-    AppRoutingModule,
-    SharedModule,
-    NgxsModule.forRoot([], {
-      developmentMode: !environment.production
-    }),
-    NgxsReduxDevtoolsPluginModule.forRoot(),
-    NgxsLoggerPluginModule.forRoot(),
-  ],
+  declarations: [AppComponent, TopNavComponent],
+  imports: [BrowserModule, HttpClientModule, AppRoutingModule, SharedModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
